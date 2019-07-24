9225fd52e26ced434cfa365e2ddd85d419f8a4c4
doori30 <rublju0070@naver.com>
2019-07-24 16:46:44 +0900

    정리

--- js/07.gallery.js
+++ js/07.gallery.js
@@ -3,14 +3,14 @@
 //var-src = $(객체).attr("src")     =>객체의 속성중 src값을 가져온다.
 //$(객체).attr("src","이미지 경로") =>객체의 crs 값을 새로운 값으로 바꾼다.
 //$(객체).trigger("click"이벤트를 댕겨줌.)=>객체의 이벤트를 실행시켜줌.
-/* $(".thumb img").click(function(){
+ $(".thumb img").click(function(){
 	var src = $(this).attr("src");
 	$(".pic img").css("opacity", 0);
 	$(".pic img").attr("src",src);
 	$(".pic img").stop().animate({"opacity": 1},500);
 });
 //$(".pic img").css("opacity": 0).attr("src",src);.stop().animate({"opacity": 1},500);
-$(".thumb img").eq(0).trigger("click"); */
+$(".thumb img").eq(0).trigger("click");
 
 $(".thumb img").click(function(){
 var src =	$(this).attr("src")
