66ee86cb6c9e03b3dfda58f2a410f49fc00453b0
doori30 <rublju0070@naver.com>
2019-07-26 10:11:28 +0900

    getter/setter

---
+++ js/10.array.js
@@ -0,0 +1,20 @@
+/* var obj = $(".box").eq(3)//리턴값이 jQ객체
+var n = $(".box").index()//리턴값이 정수 */
+
+//Gatter | Setter
+//val()  |val(객체), 
+/* var src = $(대상).attr("src")//Getter
+.$(대상).attr("src","../img/p1.jpg")//Setter
+var val = $("input[name='username']").val()//Getter
+var val = $("inpur[name='username']").val("hello")//Setter
+html() | text() 
+
+var html = $(".desc").html();//Getter
+$(".desc").html("<span>변경된 내용입니다.</span>")
+//Setter html소스를가져옴
+console.log(html);
+
+var txt = $(".desc").text();//Getter
+$(".desc").text("<span>변경된 내용입니다.</span>")
+//Setter text문자를 가져옴.
+console.log(txt); */
