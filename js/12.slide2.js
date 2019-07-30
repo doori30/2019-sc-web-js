5782d93313cc33d641203b1229b9d5334cf9eb28
doori30 <rublju0070@naver.com>
2019-07-30 10:52:44 +0900

    12slide2가로형 완성

--- js/12.slide2.js
+++ js/12.slide2.js
@@ -5,6 +5,11 @@
 				<li class="slide"><img src="../img/p5.png" alt="배너5" class="w-100"></li>
 				<li class="slide"><img src="../img/p1.png" alt="배너1" class="w-100"></li>  */
 
+	//IIFE(선언과 동시에 즉시 실행 함수.)
+	(function test(){
+		console.log("함수실행");
+	})();			
+
 var slides = [
 {"src":"../img/p1.png","link":"#"},//0
 {"src":"../img/p2.png", "link":"#"},//1
@@ -14,45 +19,102 @@ var slides = [
 {"src":"../img/p1.png", "link":"#"}//덤
 ]; 
 
-
-var Interval;
-var gap=3000; //3초에 한번씩 애니메이션 실행
-var speed=500; //애니메이션 속도가 0.5s
-var now =1;    //현재0번에서 다음 1번으로 이동시키려함.
-               //넘버가 4로 넣게 되면 시작하고 바로 4로 넘어감.
-var $slide = []; // class=slides를 생성할 slide를 담을 배열변수.저장할 공간
-
-//초기화
-for(var i in slides) {
-	//    0번 데이타에 있는 src
-	//$(".slides").append()
-	//$가 앞에 붙은 $슬라이드로 리턴함.
-	$slide[i] = $('<div class="slide"><img src="'+slides[i].src+'" class="w-100"></div>').appendTo('.slides');//0번데이타
-	$slide[i].css({"left":(i*100)+"%"});
-	if(i==0) $(".pager").append('<li class="cir-sel"></li>');
-	else if(i < slides.length-1)
-	$(".pager").append('<li class="cir"></li>');
-	//if가 0이면 cir-sel을 붙이고 100%를 생성.
-	//if가 0이 아니면 5보다 작으면 cir을 생성
-	//if가 5보다 작지 않으면 if와 else if가 적합하지 않아 작업하지 않음.
-}
-//$(".pager > li").eq(0).removeClass("cir").addClass("cir-sel");
-
-//반복, 움직임
-interavl=setInterval(function(){
-	$(".slides").stop().animate({"left":(-now*100)+"%"},speed,function(){
-		$(".pager > li").removeClass("cir-sel").addClass("cir");
-		//모든 페이저가 cir로 보여줌
-		if(now == $slide.length-1){
-			$(this).css({"left":0});
-			$(".pager > li").eq(0).removeClass("cir").addClass("cir-sel");
-			now=1;
-		}//now-1일 때, 슬라이드5와 같지 않으므로 else로 내려감.
-		 //now가 5일 때, if문 밑에 내용을 실행.
-		else {
-			$(".pager > li").eq(now).removeClass("cir").addClass("cir-sel");
-			now++;
-		}//now=5 fake->left0으로 바꿔주고 다시 1부터 시작함.
+//가로 슬라이드
+/* 1번 banner */
+(function(){
+	var interval;
+	var gap=3000; //3초에 한번씩 애니메이션 실행
+	var speed=500; //애니메이션 속도가 0.5s
+	var now =1;    //현재0번에서 다음 1번으로 이동시키려함.
+								//넘버가 4로 넣게 되면 시작하고 바로 4로 넘어감.
+	// var $slide = []; // class=slides를 생성할 slide를 담을 배열변수.저장할 공간
+	var html;
+	//초기화
+	for(var i in slides) {
+		//    0번 데이타에 있는 src
+		//$(".slides").append()
+		//$가 앞에 붙은 $슬라이드로 리턴함.
+		html='<div class="slide"><img src="'+slides[i].src+'" class="w-100"></div>';
+		/* $slide[i] = $('<div class="slide"><img src="'+slides[i].src+'" class="w-100"></div>').appendTo('.slides');//0번데이타
+		$slide[i].css({"left":(i*100)+"%"}); */
+		$(".slides").append(html);
+		if(i==0) $(".pager").append('<li class="cir-sel"></li>');
+		else if(i < slides.length-1)
+		$(".pager").append('<li class="cir"></li>');
+		//if가 0이면 cir-sel을 붙이고 100%를 생성.
+		//if가 0이 아니면 5보다 작으면 cir을 생성
+		//if가 5보다 작지 않으면 if와 else if가 적합하지 않아 작업하지 않음.
+	}
+	$(".slide").each(function(i){
+		$(this).css({"left":(i*100)+"%"});//상단 숨긴문장을 여기서 명령함.
+		
 	});
-},gap);//gap(3초)시간만큼 interval작업함.
+	//jQuery반복문.슬라이드 갯수만큼 반복이 실행됨.
 
+	/* 
+	반복문-Javascript
+	for(var i=0; i<10; i++){}
+	for(var i in the Array){}
+
+	반복문 jQuery
+	$(객체가 여러개 일 때,).each(function(index값을 준다고 해서 i로 대체적으로 표기를 함.))
+	*/
+
+	//$(".pager > li").eq(0).removeClass("cir").addClass("cir-sel");
+
+
+	//반복, 움직임
+	//        slideShow라는 함수이름...선언과 동시에 실행 되지 않는다.
+	function slideShow(){
+		$(".slides").stop().animate({"left":(-now*100)+"%"},speed,function(){
+			$(".pager > li").removeClass("cir-sel").addClass("cir");
+			//모든 페이저가 cir로 보여줌
+			if(now == slides.length-1){
+				$(this).css({"left":0});
+				$(".pager > li").eq(0).removeClass("cir").addClass("cir-sel");
+				now=1;
+			}//now-1일 때, 슬라이드5와 같지 않으므로 else로 내려감.
+			//now가 5일 때, if문 밑에 내용을 실행.
+			else {
+				$(".pager > li").eq(now).removeClass("cir").addClass("cir-sel");
+				now++;
+			}//now=5 fake->left0으로 바꿔주고 다시 1부터 시작함.
+		});
+	}
+	interval = setInterval(slideShow,gap);//gap(3초)시간만큼 interval작업함.
+
+	/* 
+	EVENT
+	-마우스를 배너에 올리면 배너의 setInterval이 멈춘다.
+	마우스를 내리면 다시 setInterval을 작동하다.
+	stop()pause()play()처럼 정지하는 동작이 없어서
+	명령으로 없애야함.
+	*/
+
+	$(".slides").mouseenter(function(){
+		clearInterval(interval);
+	});
+
+	$(".slides").mouseleave(function(){
+		interval=setInterval(slideShow,gap);
+	});
+
+	/*
+	EVENT
+	-pager >li를 클릭하면 원하는 페이지로 이동한다. 
+	*/
+	$(".pager > li").click(function(){
+		now =$(this).index();
+		slideShow();
+	});
+
+}());
+
+
+
+
+//세로 페이드 슬라이드
+/* 2번 banner */
+
+
+	
