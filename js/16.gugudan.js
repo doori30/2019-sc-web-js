9b93f99fbe9ebd852ebf7d54bfaeefc9e5dfea13
doori30 <rublju0070@naver.com>
2020-01-09 01:14:25 +0900

    구구단

---
+++ js/16.gugudan.js
@@ -0,0 +1,20 @@
+// $("#bt").click(
+//     function(){
+//         while(true){
+//             var num1 = Math.floor(Math.random()* 9) + 1; //구구단 앞숫자
+//             var num2 = Math.floor(Math.random()* 9) + 1;//구구단 뒤숫자
+//             var result = num1 * num2; //결과
+//             var gugudan =true; //조건
+//             while (gugudan) {
+//                 var answer = prompt(String(num1) + '곱하기' + String(num2) + '는?')
+//                 if(result === Number(answer)){
+//                     alert('정답');
+//                     gugudan = false;
+//                 }
+//                 else {
+//                     alert('땡')
+//                 }
+//             }
+//     }
+//     }
+// ) 
